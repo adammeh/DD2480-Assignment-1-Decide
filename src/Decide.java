b0eb6864ef17ef2612f8e2dc81db95467859e3f8
Oscar <oscarjuddch@gmail.com>
2025-01-24 19:45:34 +0100

    feat: implemented CMV_6 and created tests for method Implements #8

--- src/Decide.java
+++ src/Decide.java
@@ -14,6 +14,42 @@ class Decide{
         }
         return false;
     }
+
+    static boolean CMV_6(double[] x_coordinates, double[] y_coordinates, int NUMPOINTS, double DIST, int N_PTS){
+        if(NUMPOINTS < 3){
+            return false;
+        }
+        int index = 0;
+        while (index+N_PTS < NUMPOINTS+1){
+
+            double[] p0 = {x_coordinates[index], y_coordinates[index]};
+            double[] pn = {x_coordinates[index+N_PTS-1], y_coordinates[index+N_PTS-1]};
+            //to avoid repeated computation calculate the factors of the equation of the line P0-Pn
+            double a = pn[1] - p0[1];
+            double b = p0[0] - pn[0];
+            double c = pn[0] * p0[1] - pn[1] * p0[0];
+            //overlapping points case
+            if(a == 0 && b == 0){
+                for(int i = 1; i < x_coordinates.length -1; i++){
+                    double distance = Math.sqrt(x_coordinates[i]*x_coordinates[i] + y_coordinates[i]*y_coordinates[i]);
+                    if(distance > DIST){
+                        return true;
+                    }
+                }
+            } else {
+                for(int i = 1; i < x_coordinates.length -1; i++){
+                    double distance = Math.abs(a*x_coordinates[i] + b*y_coordinates[i] + c) / Math.sqrt(a*a + b*b);
+                    if(distance > DIST){
+                        return true;
+                    }
+                }
+            }
+            index += 1;
+
+        }
+        return false;
+    }
+
     static boolean CMV_9(double[] x_coordinates, double[] y_coordinates, int NUMPOINTS, double EPSILON,
                          int C_PTS, int D_PTS){
         if(NUMPOINTS < 5){
@@ -33,6 +69,7 @@ class Decide{
         }
         return false;
     }
+
     static double computeAngle(double p0_x, double p0_y, double p1_x, double p1_y, double p2_x, double p2_y){
         double BAx = p0_x - p1_x;
         double BAy = p0_y - p1_y;
