f4de9e8793e3688121a29099161d8f8ee2bfa639
ellmar <ellmar@kth.se>
2025-01-25 19:01:11 +0100

    feat: change LargerThan to WithCondition #16
    Helper function now takes arbitrary predicate on
    triange area. This facilitates checking for area
    both larger and smaller than comparison value

--- src/Decide.java
+++ src/Decide.java
@@ -1,9 +1,11 @@
+import java.util.function.Predicate;
+
 class Decide{
     static double triangleArea(double x0, double y0, double x1, double y1, double x2, double y2) {
         return 0.5 * Math.abs(x0 * (y1 - y2) + x1 * (y2 - y0) + x2 * (y0 - y1));
     }
     
-    static boolean existsTriangleLargerThan(double[] X, double[] Y, int NUMPOINTS, int gap1, int gap2, double area) {
+    static boolean existsTriangleWithCondition(double[] X, double[] Y, int NUMPOINTS, int gap1, int gap2, Predicate<Double> condition) {
         for ( int i = 0; i + 1 + gap1 + 1 + gap2 < NUMPOINTS; ++i ) {
             double a = triangleArea(
                 X[i],
@@ -13,7 +15,7 @@ class Decide{
                 X[i + 1 + gap1 + 1 + gap2],
                 Y[i + 1 + gap1 + 1 + gap2]
             );
-            if ( a > area ) {
+            if ( condition.test(a)) {
                 return true;
             }
         }
@@ -24,6 +26,7 @@ class Decide{
         if ( AREA1 < 0 ) {
             return false;
         }
-        return existsTriangleLargerThan(X, Y, NUMPOINTS, 1, 1, AREA1);
+        Predicate<Double> condition = a -> (a > AREA1);
+        return existsTriangleWithCondition(X, Y, NUMPOINTS, 0, 0, condition);
     }
 }
